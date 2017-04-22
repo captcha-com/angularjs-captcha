e3ad17737cec55d9e11cb84487f65db901765cbd
botdetect.support <botdetect.support@captcha.com>
2017-04-22 18:58:41 +0700

    return botdetect instance directly

--- src/angularjs-captcha.js
+++ src/angularjs-captcha.js
@@ -215,19 +215,11 @@
       this.captchaId = Captcha.getBotDetectInstance().captchaId;
     };
     
-    Captcha.botdetectInstance = null;
-    
     Captcha.getBotDetectInstance = function() {
       if (!$rootScope.captchaStyleName) {
         return null;
       }
-      
-      if (!Captcha.botdetectInstance
-          || (Captcha.botdetectInstance.captchaStyleName !== $rootScope.captchaStyleName)) {
-        Captcha.botdetectInstance = BotDetect.getInstanceByStyleName($rootScope.captchaStyleName);
-      }
-      
-      return Captcha.botdetectInstance;
+      return BotDetect.getInstanceByStyleName($rootScope.captchaStyleName);;
     };
     
     Captcha.prototype.validate = function(captchaCode) {
